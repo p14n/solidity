7885829d0bdfd07a8f4221209391b54972b55839
Dean Chapman <dean@p14n.com>
2017-11-16 21:34:25 +0000

    Try not const

--- lllc/llljs.cpp
+++ lllc/llljs.cpp
@@ -5,14 +5,14 @@
 using namespace std;
  
 extern "C" {
- const char* EMSCRIPTEN_KEEPALIVE doParseLLL(string const& _src){
+ const char* EMSCRIPTEN_KEEPALIVE doParseLLL(string _src){
   	return dev::eth::parseLLL(_src).c_str();
  } 
 }
 
-int main() {
+/*int main() {
 	string str="Passing a string";
     string rtstr = doParseLLL(str);
 	cout<<rtstr<<endl;
 	return 0;
-}
+}*/
